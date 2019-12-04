e3a3b1b7b7d93dd2e19c5442837f36fd55b22ca9
neoNovadry <36753804+keyboard2769@users.noreply.github.com>
2019-12-04 16:23:42 +0800

    Add files via upload

--- CaseSimplePID/CaseSimplePID.java
+++ CaseSimplePID/CaseSimplePID.java
@@ -10,14 +10,20 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.GridLayout;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
+import javax.swing.JComponent;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JSeparator;
 import javax.swing.JTabbedPane;
 import javax.swing.JTextField;
 import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
 import processing.core.PApplet;
 
 public class CaseSimplePID extends PApplet{
@@ -25,6 +31,13 @@ public class CaseSimplePID extends PApplet{
   private static PApplet self=null;
   
   //=== action
+  
+  static final MouseAdapter O_INPUT_BOX_LISTENER = new MouseAdapter() {
+    @Override public void mouseReleased(MouseEvent me) {
+      String lpD = ccGetStringByInputBox("??", "000");
+      System.err.println("not_yet:"+lpD);
+    }//+++
+  };//***
   
   //=== swing
   
@@ -43,15 +56,50 @@ public class CaseSimplePID extends PApplet{
   static final Runnable O_SWING_INIT = new Runnable() {
     @Override public void run() {
       
+      //-- restyle
+      try {
+        UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+      } catch (Exception e) {
+        System.err.println("O_SWING_INIT::"+e.getMessage());
+        System.exit(-1);
+      }//..?
+      
       //-- setting pane
       //-- setting pane ** target lane
+      ccSetupInputBox(O_TEMP_TARGET_TB, 64, 22);
       JPanel lpTargetLane = new JPanel(new FlowLayout(FlowLayout.LEADING));
       lpTargetLane.setBorder(BorderFactory
-        .createTitledBorder(BorderFactory.createEtchedBorder(), "target"));
-      lpTargetLane.add(new JLabel("value[%]:"));
+        .createTitledBorder(BorderFactory.createEtchedBorder(), "Target"));
+      lpTargetLane.add(new JLabel("value[`C]:"));
+      lpTargetLane.add(O_TEMP_TARGET_TB);
+      //-- setting pane ** range lane
+      ccSetupInputBox(O_TEMP_PORT_TB, 48, 22);
+      ccSetupInputBox(O_TEMP_DEAD_TB, 48, 22);
+      JPanel lpRangeLane = new JPanel(new FlowLayout(FlowLayout.LEADING));
+      lpRangeLane.setBorder(BorderFactory
+        .createTitledBorder(BorderFactory.createEtchedBorder(), "Range"));
+      lpRangeLane.add(new JLabel("proportion[?]:"));
+      lpRangeLane.add(O_TEMP_PORT_TB);
+      lpRangeLane.add(new JSeparator(JSeparator.VERTICAL));
+      lpRangeLane.add(new JLabel("dead[?]:"));
+      lpRangeLane.add(O_TEMP_DEAD_TB);
+      //-- setting pane ** adjust lane
+      ccSetupInputBox(O_TEMP_SAMP_TB, 48, 22);
+      ccSetupInputBox(O_TEMP_ADJT_TB, 48, 22);
+      JPanel lpAdjustLane = new JPanel(new FlowLayout(FlowLayout.LEADING));
+      lpAdjustLane.setBorder(BorderFactory
+        .createTitledBorder(BorderFactory.createEtchedBorder(), "Adjust"));
+      lpAdjustLane.add(new JLabel("sampling[s]:"));
+      lpAdjustLane.add(O_TEMP_SAMP_TB);
+      lpAdjustLane.add(new JSeparator(JSeparator.VERTICAL));
+      lpAdjustLane.add(new JLabel("adjusting[s]:"));
+      lpAdjustLane.add(O_TEMP_ADJT_TB);
+      
       //-- setting pane ** pack
       JPanel lpSettingPane = new JPanel(new GridLayout(3, 1, 1, 1));
       lpSettingPane.add(lpTargetLane);
+      lpSettingPane.add(lpRangeLane);
+      lpSettingPane.add(lpAdjustLane);
       
       //-- dummy pane
       JPanel lpDummyPane = new JPanel(new BorderLayout(1, 1));
@@ -66,6 +114,7 @@ public class CaseSimplePID extends PApplet{
       O_FRAME.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
       O_FRAME.getContentPane().add(lpContentPane);
       O_FRAME.setPreferredSize(new Dimension(320, 240));
+      O_FRAME.setResizable(false);
       O_FRAME.setLocation(
         self.frame.getLocation().x+self.frame.getWidth()+5,
         self.frame.getLocation().y
@@ -97,6 +146,16 @@ public class CaseSimplePID extends PApplet{
     pxTarget.setDisabledTextColor(Color.DARK_GRAY);
     pxTarget.setHorizontalAlignment(JTextField.RIGHT);
     pxTarget.setPreferredSize(new Dimension(pxW, pxH));
+    pxTarget.addMouseListener(O_INPUT_BOX_LISTENER);
+  }//+++
+  
+  static final
+  String ccGetStringByInputBox(String pxBrief, String pxDefault){
+    if(!SwingUtilities.isEventDispatchThread()){return "";}
+    return JOptionPane.showInputDialog(
+      O_FRAME,
+      pxBrief, pxDefault
+    );
   }//+++
 
   //=== overridden
@@ -173,6 +232,11 @@ public class CaseSimplePID extends PApplet{
     exit();
   }//+++
   
+  private boolean ssIsValidString(String pxLine){
+    if(pxLine==null){return false;}
+    return !pxLine.isEmpty();
+  }//+++
+  
   //=== real
   
   class ZcReal{
@@ -190,11 +254,18 @@ public class CaseSimplePID extends PApplet{
   //=== controller
   
   class ZcController{
+    float[] cmDesProcessHistory;
+    float cmProcessValue,cmProcessMinimum,cmProcessMaximum;
+    float cmTarget,cmShiftedTarget;
+    float cmDeadPositive, cmDeadNegative;
+    float cmProportionPositive, cmProportionNegative;
+    float cmSamplingDead,cmAdjustWidth;
+    float cmAnalogOutput;
+    //--
     void ccRun(){}
     //--
     void ccSetDead(){}
     void ccSetProportion(){}
-    
     //--
     void ccSetProcessValue(){}
     void ccSetTargetValue(){}
@@ -202,12 +273,11 @@ public class CaseSimplePID extends PApplet{
     float ccGetAnalogoutput(){return 0f;}
     boolean ccGetPositiveOutput(){return false;}
     boolean ccGetNegativeOutput(){return false;}
-    
   }//***
 
   //=== entry
   
-  public static void main(String[] args) {
+  public static void main(String[] args){
     PApplet.main(CaseSimplePID.class.getCanonicalName());
   }//!!!
 
