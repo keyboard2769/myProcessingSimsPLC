de270f3b46ab174011e956683070fd7939175195
neoNovadry <36753804+keyboard2769@users.noreply.github.com>
2019-12-11 16:29:45 +0800

    Add files via upload

--- CaseSimplePID/CaseSimplePID.java
+++ CaseSimplePID/CaseSimplePID.java
@@ -5,16 +5,18 @@
 
 package ppptest;
 
+import processing.core.PApplet;
+
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.GridLayout;
+import java.awt.Rectangle;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
-import javax.swing.JComponent;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
@@ -24,7 +26,6 @@ import javax.swing.JTabbedPane;
 import javax.swing.JTextField;
 import javax.swing.SwingUtilities;
 import javax.swing.UIManager;
-import processing.core.PApplet;
 
 public class CaseSimplePID extends PApplet{
   
@@ -162,6 +163,14 @@ public class CaseSimplePID extends PApplet{
   
   int pbRoller = 0;
   
+  EcElement pbBurnerICON          = new EcElement();
+  EcElement pbBurnerDegreeClosePL = new EcElement("-");
+  EcElement pbBurnerDegreeTB      = new EcElement();
+  EcElement pbBurnerDegreeOpenPL  = new EcElement("+");
+  
+  EcElement pbDryerICON     = new EcElement();
+  EcElement pbDryerDegreeTB = new EcElement();
+  
   @Override public void setup() {
     
     //-- pre setting
@@ -172,6 +181,41 @@ public class CaseSimplePID extends PApplet{
     
     //-- replace setting
     frameRate(16);noStroke();textAlign(LEFT, TOP);ellipseMode(CENTER);
+    
+    //-- local ui
+    int lpPotentialW, lpPotentialH;
+    
+    //-- local ui ** burner
+    lpPotentialW = 48;
+    lpPotentialH = 18;
+    pbBurnerDegreeClosePL.cmRegion.setBounds(25,25,lpPotentialH,lpPotentialH);
+    pbBurnerDegreeTB.cmOffColor=0xFFCCCCCC;
+    pbBurnerDegreeTB.cmRegion.setSize(lpPotentialW, lpPotentialH);
+    pbBurnerDegreeTB.ccFollowH(pbBurnerDegreeClosePL, 2);
+    pbBurnerDegreeOpenPL.cmRegion.setSize(lpPotentialH, lpPotentialH);
+    pbBurnerDegreeOpenPL.ccFollowH(pbBurnerDegreeTB, 2);
+    lpPotentialW =
+      ccGetEndX(pbBurnerDegreeOpenPL.cmRegion)
+       - pbBurnerDegreeClosePL.cmRegion.x;
+    lpPotentialH = 
+      lpPotentialW *2 /3;
+    pbBurnerICON.cmRegion.setSize(lpPotentialW, lpPotentialH);
+    pbBurnerICON.ccFollowV(pbBurnerDegreeClosePL, 2);
+    
+    //-- local ui ** dryer
+    lpPotentialH = lpPotentialW;
+    lpPotentialW = lpPotentialH*2;
+    pbDryerICON.cmRegion.setSize(lpPotentialW, lpPotentialH);
+    pbDryerICON.ccFollowE(pbBurnerICON, 5);
+    pbDryerICON.cmRegion.y -= pbBurnerICON.cmRegion.height/4;
+    lpPotentialW = 54;
+    lpPotentialH = 22;
+    pbDryerDegreeTB.cmOffColor=0xFFCCCCCC;
+    pbDryerDegreeTB.cmRegion.setBounds(
+      ccGetCenterX(pbDryerICON.cmRegion),
+      ccGetCenterY(pbDryerICON.cmRegion),
+      lpPotentialW, lpPotentialH
+    );
     
     //-- post setting
     SwingUtilities.invokeLater(O_SWING_INIT);
@@ -184,6 +228,19 @@ public class CaseSimplePID extends PApplet{
     //-- pre drawing
     background(0);
     pbRoller++;pbRoller&=0x0f;
+    
+    //-- local ui
+    ccDrawAsLabel(pbBurnerDegreeClosePL);
+    ccDrawAsValueBox(pbBurnerDegreeTB, mouseX, "%");
+    ccDrawAsLabel(pbBurnerDegreeOpenPL);
+    ccDrawAsBlower(pbBurnerICON);
+    ccDrawAsDryer(pbDryerICON);
+    ccDrawAsValueBox(pbDryerDegreeTB, mouseY, "`C");
+    
+    //-- controller ** linear
+    
+    //-- controller ** indication
+    ccSurroundText("we will we will \n rock you!", mouseX, mouseY);
     
     //-- updating
     fill(0xFF);
@@ -225,17 +282,173 @@ public class CaseSimplePID extends PApplet{
   
   //=== utility
   
-  private void ssPover(){
+  void ssPover(){
     
     //-- default
     println("--exit <- ");
     exit();
   }//+++
   
-  private boolean ssIsValidString(String pxLine){
+  boolean ssIsValidString(String pxLine){
     if(pxLine==null){return false;}
     return !pxLine.isEmpty();
   }//+++
+  
+  void ccSurroundText(String pxText, int pxX, int pxY){
+    if(pxText==null){return;}
+    final int lpMargin = 2;
+    int lpWidth = ceil(textWidth(pxText)) + lpMargin*2;
+    int lpReturnCount = 1;
+    for(char it : pxText.toCharArray()){
+      if(it == '\r' || it == '\n'){lpReturnCount++;}
+    }//..~
+    int lpHeight = ceil(textAscent()+textDescent())*lpReturnCount + lpMargin*2;
+    pushStyle();
+    {
+      fill(0x55555555);
+      stroke(0xFFEEEEEE);
+      rect(pxX, pxY, lpWidth, lpHeight);
+      fill(0xFFEEEEEE);
+      text(pxText, pxX+lpMargin, pxY+lpMargin);
+    }
+    popStyle();
+  }//+++
+  
+  //=== local ui
+  
+  int ccGetCenterX(Rectangle pxTarget){
+    return pxTarget.x+pxTarget.width/2;
+  }//+++
+  
+  int ccGetCenterY(Rectangle pxTarget){
+    return pxTarget.y+pxTarget.height/2;
+  }//+++
+  
+  int ccGetEndX(Rectangle pxTarget){
+    return pxTarget.x + pxTarget.width;
+  }//+++
+  
+  int ccGetEndY(Rectangle pxTarget){
+    return pxTarget.y + pxTarget.height;
+  }//+++
+  
+  class EcElement{
+    Rectangle cmRegion=new Rectangle(8, 8, 8, 8);
+    String cmText="<text>";
+    boolean cmActivity=false;
+    int cmOnColor    = 0xFFEEEE33;
+    int cmOffColor   = 0xFF555555;
+    int cmFrontColor = 0xFF333333;
+    int cmBackColor  = 0xFF777777;
+    //===
+    public EcElement(){}//++!
+    public EcElement(String pxText){
+      this();
+      cmText=pxText;
+    }//++!
+    public EcElement(String pxText, boolean pxAct){
+      this(pxText);
+      cmActivity=pxAct;
+    }//++!
+    //===
+    void ccFollowH(EcElement pxTarget, int pxOffset){
+      cmRegion.x=ccGetEndX(pxTarget.cmRegion)+pxOffset;
+      cmRegion.y=pxTarget.cmRegion.y;
+    }//+++
+    void ccFollowV(EcElement pxTarget, int pxOffset){
+      cmRegion.x=pxTarget.cmRegion.x;
+      cmRegion.y=ccGetEndY(pxTarget.cmRegion)+pxOffset;
+    }//+++
+    void ccFollowE(EcElement pxTarget, int pxOffset){
+      cmRegion.x=ccGetEndX(pxTarget.cmRegion)+pxOffset;
+      cmRegion.y=ccGetCenterY(pxTarget.cmRegion) - cmRegion.height/2;
+    }//+++
+    void ccFollowS(EcElement pxTarget, int pxOffset){
+      cmRegion.x=ccGetCenterX(pxTarget.cmRegion) - cmRegion.width/2;
+      cmRegion.y=ccGetEndY(pxTarget.cmRegion)+pxOffset;
+    }//+++
+  }//***
+  
+  void ccDrawAsLabel(EcElement pxTarget){
+    pushStyle();
+    {
+      fill(pxTarget.cmActivity?pxTarget.cmOnColor:pxTarget.cmOffColor);
+      rect(
+        pxTarget.cmRegion.x, pxTarget.cmRegion.y,
+        pxTarget.cmRegion.width, pxTarget.cmRegion.height
+      );
+      textAlign(CENTER, CENTER);
+      fill(pxTarget.cmFrontColor);
+      text(pxTarget.cmText,
+        ccGetCenterX(pxTarget.cmRegion),
+        ccGetCenterY(pxTarget.cmRegion)
+      );
+    }
+    popStyle();
+  }//+++
+  
+  void ccDrawAsValueBox(EcElement pxTarget, int pxValue, String pxUnit){
+    pxTarget.cmText=String.format("%04d %s", pxValue, pxUnit);
+    ccDrawAsLabel(pxTarget);
+  }//+++
+  
+  void ccDrawAsBlower(EcElement pxTarget){
+    fill(pxTarget.cmBackColor);
+    ellipse(
+      pxTarget.cmRegion.x + pxTarget.cmRegion.height/2,
+      pxTarget.cmRegion.y + pxTarget.cmRegion.height/2,
+      pxTarget.cmRegion.height,
+      pxTarget.cmRegion.height
+    );
+    rect(
+      pxTarget.cmRegion.x + pxTarget.cmRegion.height/2,
+      pxTarget.cmRegion.y,
+      pxTarget.cmRegion.width - pxTarget.cmRegion.height/2,
+      pxTarget.cmRegion.height/2
+    );
+  }//+++
+  
+  void ccDrawAsDryer(EcElement pxTarget){
+    final int lpDevide      = 6;
+    final int lpBarrelWestX = pxTarget.cmRegion.width/lpDevide;
+    final int lpBarrelEastX = pxTarget.cmRegion.width*(lpDevide-2) / lpDevide;
+    final int lpBarrelW     = pxTarget.cmRegion.width*(lpDevide-1) / lpDevide;
+    final int lpSupporterW  = pxTarget.cmRegion.height /16;
+    final int lpBerrelH     = pxTarget.cmRegion.height *2 /3;
+    final int lpSupporterG  = (pxTarget.cmRegion.height - lpBerrelH)/2;
+    final int lpDuctG       = lpSupporterW;
+    fill(pxTarget.cmBackColor);
+    rect(//..barrel
+      pxTarget.cmRegion.x,
+      pxTarget.cmRegion.y + lpSupporterG,
+      lpBarrelW,
+      lpBerrelH
+    );
+    rect(//..supporter west
+      pxTarget.cmRegion.x + lpBarrelWestX,
+      pxTarget.cmRegion.y,
+      lpSupporterW,
+      pxTarget.cmRegion.height
+    );
+    rect(//..supporter east
+      pxTarget.cmRegion.x + lpBarrelEastX,
+      pxTarget.cmRegion.y,
+      lpSupporterW,
+      pxTarget.cmRegion.height
+    );
+    triangle(//..duct
+      pxTarget.cmRegion.x +lpBarrelW +lpDuctG,
+      pxTarget.cmRegion.y,
+      //--
+      ccGetEndX(pxTarget.cmRegion),
+      pxTarget.cmRegion.y,
+      //--
+      pxTarget.cmRegion.x +lpBarrelW +lpDuctG,
+      ccGetCenterY(pxTarget.cmRegion)
+    );
+  }//+++
+  
+  //[todo]::void ccDrawAsLine(ZcController pxTarget){}//+++
   
   //=== real
   
