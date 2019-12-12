af93ec932bf0e6883dacc700a0145cf267ed1422
neoNovadry <36753804+keyboard2769@users.noreply.github.com>
2019-12-12 11:58:08 +0800

    Add files via upload

--- CaseSimplePID/CaseSimplePID.java
+++ CaseSimplePID/CaseSimplePID.java
@@ -13,17 +13,23 @@ import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.GridLayout;
 import java.awt.Rectangle;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JSeparator;
 import javax.swing.JTabbedPane;
+import javax.swing.JTextArea;
 import javax.swing.JTextField;
+import javax.swing.JToggleButton;
 import javax.swing.SwingUtilities;
 import javax.swing.UIManager;
 
@@ -31,54 +37,120 @@ public class CaseSimplePID extends PApplet{
   
   private static PApplet self=null;
   
+  //=== swing
+  
+  //-- swing ** top
+  static final JFrame O_FRAME = new JFrame(CaseSimplePID.class.getName());
+  
+  //-- swing ** operation ** combust
+  static final JToggleButton     O_OPRT_BFC_SW = new JToggleButton("FIRE");
+  static final JButton           O_OPRT_BUC_SW = new JButton("-");
+  static final JButton           O_OPRT_BUO_SW = new JButton("+");
+  static final JComboBox<String> O_OPRT_BMD_NT = new JComboBox<>(new String[]{
+    "AUTO","DISAB","MANUAL"
+  });
+  //-- swing ** operation ** flux
+  static final JButton    O_OPRT_FDD_SW = new JButton("DEC");
+  static final JButton    O_OPRT_FPP_SW = new JButton("INC");
+  static final JTextField O_OPRT_FXX_TB = new JTextField("000.0");
+  //-- swing ** operation ** cooling
+  static final JTextField        O_OPRT_MVL_TB = new JTextField("000.0");
+  static final JComboBox<String> O_OPRT_MVR_NT = new JComboBox<>(new String[]{
+    "AUTO","DISAB","FORCE"
+  });
+  
+  //-- swing ** temperature controller
+  static final JTextField O_TEMP_TARGET_TB = new JTextField("000.0");
+  static final JTextField O_TEMP_DEAD_TB   = new JTextField("000.0");
+  static final JTextField O_TEMP_PORT_TB   = new JTextField("000.0");
+  static final JTextField O_TEMP_SAMP_TB   = new JTextField("000.0");
+  static final JTextField O_TEMP_ADJT_TB   = new JTextField("000.0");
+  
+  //-- swing ** degree controller
+  
   //=== action
   
-  static final MouseAdapter O_INPUT_BOX_LISTENER = new MouseAdapter() {
-    @Override public void mouseReleased(MouseEvent me) {
-      String lpD = ccGetStringByInputBox("??", "000");
-      System.err.println("not_yet:"+lpD);
+  static final ActionListener O_NOTCH_LISTENER = new ActionListener() {
+    @Override public void actionPerformed(ActionEvent ae) {
+      Object lpSouce = ae.getSource();
+      System.err.println(".actionPerformed():not_yet:"+lpSouce.toString());
     }//+++
   };//***
   
-  //=== swing
+  static final MouseAdapter O_MOMENTARY_LISTENER = new MouseAdapter() {
+    @Override public void mousePressed(MouseEvent me) {
+      System.err.println(".mousePressed():not_yet");
+    }//+++
+    @Override public void mouseReleased(MouseEvent me) {
+      System.err.println(".mouseReleased()::not_yet");
+    }//+++
+  };//***
   
-  static final JFrame O_FRAME = new JFrame(CaseSimplePID.class.getName());
-  
-  static final JTextField O_TEMP_TARGET_TB = new JTextField("0.0");
-  
-  static final JTextField O_TEMP_DEAD_TB = new JTextField("0.0");
-  
-  static final JTextField O_TEMP_PORT_TB = new JTextField("0.0");
-  
-  static final JTextField O_TEMP_SAMP_TB = new JTextField("0.0");
-  
-  static final JTextField O_TEMP_ADJT_TB = new JTextField("0.0");
+  static final MouseAdapter O_INPUT_BOX_LISTENER = new MouseAdapter() {
+    @Override public void mouseReleased(MouseEvent me) {
+      Object lpSource = me.getSource();
+      String lpD = ccGetStringByInputBox("??", "000");
+      System.err.println(lpSource.toString()+":not_yet:"+lpD);
+    }//+++
+  };//***
   
   static final Runnable O_SWING_INIT = new Runnable() {
     @Override public void run() {
       
       //-- restyle
-      try {
+      try{
         UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-      } catch (Exception e) {
+      }catch(Exception e){
         System.err.println("O_SWING_INIT::"+e.getMessage());
         System.exit(-1);
       }//..?
       
+      //-- operate pane
+      //-- operate pane ** combust lane
+      O_OPRT_BFC_SW.addActionListener(O_NOTCH_LISTENER);
+      O_OPRT_BMD_NT.addActionListener(O_NOTCH_LISTENER);
+      ccSetupMomentarySwitch(O_OPRT_BUC_SW);
+      ccSetupMomentarySwitch(O_OPRT_BUO_SW);
+      JPanel lpCombustLane = ccCreateLane("Combust");
+      lpCombustLane.add(O_OPRT_BFC_SW);
+      lpCombustLane.add(new JSeparator(JSeparator.VERTICAL));
+      lpCombustLane.add(O_OPRT_BMD_NT);
+      lpCombustLane.add(O_OPRT_BUC_SW);
+      lpCombustLane.add(O_OPRT_BUO_SW);
+      //-- operate pane ** flux lane
+      ccSetupInputBox(O_OPRT_FXX_TB, 48, 22);
+      ccSetupMomentarySwitch(O_OPRT_FDD_SW);
+      ccSetupMomentarySwitch(O_OPRT_FPP_SW);
+      JPanel lpFluxLane = ccCreateLane("Flux");
+      lpFluxLane.add(O_OPRT_FDD_SW);
+      lpFluxLane.add(O_OPRT_FPP_SW);
+      lpFluxLane.add(new JSeparator(JSeparator.VERTICAL));
+      lpFluxLane.add(new JLabel("step[tph]:"));
+      lpFluxLane.add(O_OPRT_FXX_TB);
+      //-- operate pane ** cooling lane
+      O_OPRT_MVR_NT.addActionListener(O_NOTCH_LISTENER);
+      ccSetupInputBox(O_OPRT_MVL_TB, 48, 22);
+      JPanel lpCoolingLane = ccCreateLane("Cooling");
+      lpCoolingLane.add(O_OPRT_MVR_NT);
+      lpCoolingLane.add(new JSeparator(JSeparator.VERTICAL));
+      lpCoolingLane.add(new JLabel("limit[`C]:"));
+      lpCoolingLane.add(O_OPRT_MVL_TB);
+      //-- operate pane ** pack
+      JPanel lpOperatingPane = new JPanel(new GridLayout(3, 1, 1, 1));
+      lpOperatingPane.add(lpCombustLane);
+      lpOperatingPane.add(lpFluxLane);
+      lpOperatingPane.add(lpCoolingLane);      
+      
       //-- setting pane
       //-- setting pane ** target lane
       ccSetupInputBox(O_TEMP_TARGET_TB, 64, 22);
-      JPanel lpTargetLane = new JPanel(new FlowLayout(FlowLayout.LEADING));
-      lpTargetLane.setBorder(BorderFactory
-        .createTitledBorder(BorderFactory.createEtchedBorder(), "Target"));
+      JPanel lpTargetLane = ccCreateLane("Target");
       lpTargetLane.add(new JLabel("value[`C]:"));
       lpTargetLane.add(O_TEMP_TARGET_TB);
       //-- setting pane ** range lane
       ccSetupInputBox(O_TEMP_PORT_TB, 48, 22);
       ccSetupInputBox(O_TEMP_DEAD_TB, 48, 22);
-      JPanel lpRangeLane = new JPanel(new FlowLayout(FlowLayout.LEADING));
-      lpRangeLane.setBorder(BorderFactory
-        .createTitledBorder(BorderFactory.createEtchedBorder(), "Range"));
+      JPanel lpRangeLane = ccCreateLane("Range");
       lpRangeLane.add(new JLabel("proportion[?]:"));
       lpRangeLane.add(O_TEMP_PORT_TB);
       lpRangeLane.add(new JSeparator(JSeparator.VERTICAL));
@@ -87,29 +159,32 @@ public class CaseSimplePID extends PApplet{
       //-- setting pane ** adjust lane
       ccSetupInputBox(O_TEMP_SAMP_TB, 48, 22);
       ccSetupInputBox(O_TEMP_ADJT_TB, 48, 22);
-      JPanel lpAdjustLane = new JPanel(new FlowLayout(FlowLayout.LEADING));
-      lpAdjustLane.setBorder(BorderFactory
-        .createTitledBorder(BorderFactory.createEtchedBorder(), "Adjust"));
+      JPanel lpAdjustLane = ccCreateLane("Adjust");
       lpAdjustLane.add(new JLabel("sampling[s]:"));
       lpAdjustLane.add(O_TEMP_SAMP_TB);
       lpAdjustLane.add(new JSeparator(JSeparator.VERTICAL));
       lpAdjustLane.add(new JLabel("adjusting[s]:"));
       lpAdjustLane.add(O_TEMP_ADJT_TB);
-      
       //-- setting pane ** pack
       JPanel lpSettingPane = new JPanel(new GridLayout(3, 1, 1, 1));
       lpSettingPane.add(lpTargetLane);
       lpSettingPane.add(lpRangeLane);
       lpSettingPane.add(lpAdjustLane);
       
-      //-- dummy pane
-      JPanel lpDummyPane = new JPanel(new BorderLayout(1, 1));
-      lpDummyPane.add(new JButton("=D="));
+      //-- info pane
+      JPanel lpInfoPane = new JPanel(new BorderLayout(1, 1));
+      JTextArea lpInfoArea = new JTextArea("=D=");
+      lpInfoArea.setEditable(false);
+      lpInfoArea.setEnabled(false);
+      lpInfoArea.append("du you reall yknow?");
+      lpInfoArea.append("maybe not?");
+      lpInfoPane.add(new JScrollPane(lpInfoArea));
       
       //-- tab
       JTabbedPane lpContentPane = new JTabbedPane();
-      lpContentPane.add("setting", lpSettingPane);
-      lpContentPane.add("dummy", lpDummyPane);
+      lpContentPane.add("Operating", lpOperatingPane);
+      lpContentPane.add("Temperature", lpSettingPane);
+      lpContentPane.add("Info", lpInfoPane);
       
       //-- frame
       O_FRAME.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
@@ -150,8 +225,20 @@ public class CaseSimplePID extends PApplet{
     pxTarget.addMouseListener(O_INPUT_BOX_LISTENER);
   }//+++
   
-  static final
-  String ccGetStringByInputBox(String pxBrief, String pxDefault){
+  static final void ccSetupMomentarySwitch(JButton pxTarget){
+    pxTarget.setFocusPainted(false);
+    pxTarget.setBackground(Color.decode("#EEEECC"));
+    pxTarget.addMouseListener(O_MOMENTARY_LISTENER);
+  }//+++
+  
+  static final JPanel ccCreateLane(String pxTitle){
+    JPanel lpRes = new JPanel(new FlowLayout(FlowLayout.LEADING));
+    lpRes.setBorder(BorderFactory
+      .createTitledBorder(BorderFactory.createEtchedBorder(), pxTitle));
+    return lpRes;
+  }//+++
+  
+  static final String ccGetStringByInputBox(String pxBrief, String pxDefault){
     if(!SwingUtilities.isEventDispatchThread()){return "";}
     return JOptionPane.showInputDialog(
       O_FRAME,
@@ -161,15 +248,35 @@ public class CaseSimplePID extends PApplet{
 
   //=== overridden
   
+  //-- system
   int pbRoller = 0;
   
-  EcElement pbBurnerICON          = new EcElement();
-  EcElement pbBurnerDegreeClosePL = new EcElement("-");
-  EcElement pbBurnerDegreeTB      = new EcElement();
-  EcElement pbBurnerDegreeOpenPL  = new EcElement("+");
+  //-- local ui
+  EcElement mnBurnerICON          = new EcElement();
+  EcElement mnBurnerDegreeClosePL = new EcElement("-");
+  EcElement mnBurnerDegreeTB      = new EcElement();
+  EcElement mnBurnerDegreeOpenPL  = new EcElement("+");
+  EcElement mnBurnerFirePL    = new EcElement(" ");
+  EcElement mnDryerICON       = new EcElement();
+  EcElement mnDryerDegreeTB   = new EcElement();
+  EcElement mnDryerFluxTB     = new EcElement();
+  EcElement mnCoolingDamperPL = new EcElement(" ");
   
-  EcElement pbDryerICON     = new EcElement();
-  EcElement pbDryerDegreeTB = new EcElement();
+  //-- operated
+  static volatile boolean mnBurnerManualCloseFLG = false;
+  static volatile boolean mnBurnerManualOpenFLG  = false;
+  static volatile boolean mnBurnerAutoFLG        = false;
+  static volatile boolean mnBurnerManualFLG      = false;
+  static volatile boolean mnBurnerFireFLG        = false;
+  
+  //-- simulated
+  boolean dcBurnerCloseMV     = false;
+  boolean dcBurnerOpenMV      = false;
+  boolean dcBurnerOnFire      = false;
+  boolean dcCoolingDamperMV   = false;
+  float   dcDryerFlux         = 160.0f;
+  ZcReal dcDryerTemperature = new ZcReal(27.0f);
+  ZcReal dcAirTemperature   = new ZcReal(27.0f,true);
   
   @Override public void setup() {
     
@@ -188,33 +295,44 @@ public class CaseSimplePID extends PApplet{
     //-- local ui ** burner
     lpPotentialW = 48;
     lpPotentialH = 18;
-    pbBurnerDegreeClosePL.cmRegion.setBounds(25,25,lpPotentialH,lpPotentialH);
-    pbBurnerDegreeTB.cmOffColor=0xFFCCCCCC;
-    pbBurnerDegreeTB.cmRegion.setSize(lpPotentialW, lpPotentialH);
-    pbBurnerDegreeTB.ccFollowH(pbBurnerDegreeClosePL, 2);
-    pbBurnerDegreeOpenPL.cmRegion.setSize(lpPotentialH, lpPotentialH);
-    pbBurnerDegreeOpenPL.ccFollowH(pbBurnerDegreeTB, 2);
+    mnBurnerDegreeClosePL.cmRegion.setBounds(5,25,lpPotentialH,lpPotentialH);
+    mnBurnerDegreeTB.cmOffColor=0xFFCCCCCC;
+    mnBurnerDegreeTB.cmRegion.setSize(lpPotentialW, lpPotentialH);
+    mnBurnerDegreeTB.ccFollowH(mnBurnerDegreeClosePL, 2);
+    mnBurnerDegreeOpenPL.cmRegion.setSize(lpPotentialH, lpPotentialH);
+    mnBurnerDegreeOpenPL.ccFollowH(mnBurnerDegreeTB, 2);
     lpPotentialW =
-      ccGetEndX(pbBurnerDegreeOpenPL.cmRegion)
-       - pbBurnerDegreeClosePL.cmRegion.x;
+      ccGetEndX(mnBurnerDegreeOpenPL.cmRegion)
+       - mnBurnerDegreeClosePL.cmRegion.x;
     lpPotentialH = 
       lpPotentialW *2 /3;
-    pbBurnerICON.cmRegion.setSize(lpPotentialW, lpPotentialH);
-    pbBurnerICON.ccFollowV(pbBurnerDegreeClosePL, 2);
+    mnBurnerICON.cmRegion.setSize(lpPotentialW, lpPotentialH);
+    mnBurnerICON.ccFollowV(mnBurnerDegreeClosePL, 2);
+    mnBurnerFirePL.cmRegion.setLocation(ccGetEndX(mnBurnerICON.cmRegion) -mnBurnerFirePL.cmRegion.width -2,
+      ccGetCenterY(mnBurnerICON.cmRegion) -mnBurnerFirePL.cmRegion.height -2
+    );
     
     //-- local ui ** dryer
     lpPotentialH = lpPotentialW;
     lpPotentialW = lpPotentialH*2;
-    pbDryerICON.cmRegion.setSize(lpPotentialW, lpPotentialH);
-    pbDryerICON.ccFollowE(pbBurnerICON, 5);
-    pbDryerICON.cmRegion.y -= pbBurnerICON.cmRegion.height/4;
+    mnDryerICON.cmRegion.setSize(lpPotentialW, lpPotentialH);
+    mnDryerICON.ccFollowE(mnBurnerICON, 5);
+    mnDryerICON.cmRegion.y -= mnBurnerICON.cmRegion.height/4;
     lpPotentialW = 54;
     lpPotentialH = 22;
-    pbDryerDegreeTB.cmOffColor=0xFFCCCCCC;
-    pbDryerDegreeTB.cmRegion.setBounds(
-      ccGetCenterX(pbDryerICON.cmRegion),
-      ccGetCenterY(pbDryerICON.cmRegion),
+    mnDryerDegreeTB.cmOffColor=0xFFEECCCC;
+    mnDryerDegreeTB.cmRegion.setBounds(ccGetCenterX(mnDryerICON.cmRegion),
+      ccGetCenterY(mnDryerICON.cmRegion),
       lpPotentialW, lpPotentialH
+    );
+    lpPotentialW = 60;
+    lpPotentialH = 22;
+    mnDryerFluxTB.cmOffColor=0xFFEEEECC;
+    mnDryerFluxTB.cmRegion.setSize(lpPotentialW, lpPotentialH);
+    mnDryerFluxTB.ccFollowH(mnDryerDegreeTB, 10);
+    mnCoolingDamperPL.cmRegion.setLocation(
+      mnDryerICON.cmRegion.x+mnDryerICON.cmRegion.width*5/6+8,
+      mnDryerICON.cmRegion.y+2
     );
     
     //-- post setting
@@ -230,17 +348,20 @@ public class CaseSimplePID extends PApplet{
     pbRoller++;pbRoller&=0x0f;
     
     //-- local ui
-    ccDrawAsLabel(pbBurnerDegreeClosePL);
-    ccDrawAsValueBox(pbBurnerDegreeTB, mouseX, "%");
-    ccDrawAsLabel(pbBurnerDegreeOpenPL);
-    ccDrawAsBlower(pbBurnerICON);
-    ccDrawAsDryer(pbDryerICON);
-    ccDrawAsValueBox(pbDryerDegreeTB, mouseY, "`C");
-    
-    //-- controller ** linear
-    
-    //-- controller ** indication
-    ccSurroundText("we will we will \n rock you!", mouseX, mouseY);
+    //-- local ui ** burner
+    ccDrawAsLabel(mnBurnerDegreeClosePL);
+    ccDrawAsValueBox(mnBurnerDegreeTB, mouseX, "%");
+    ccDrawAsLabel(mnBurnerDegreeOpenPL);
+    ccDrawAsBlower(mnBurnerICON);
+    ccDrawAsLabel(mnBurnerFirePL);
+    //-- local ui ** burner
+    ccDrawAsDryer(mnDryerICON);
+    ccDrawAsValueBox(mnDryerDegreeTB, mouseY, "`C");
+    ccDrawAsValueBox(mnDryerFluxTB, mouseY, "tph");
+    ccDrawAsLabel(mnCoolingDamperPL);
+    //-- local ui ** controller
+    ccSurroundText("TPCTRL\n..this is a dummy\nnot_yet!!",   5, 125);
+    ccSurroundText("BDCTRL\n..this is a dummy\nnot_yet!!", 165, 125);
     
     //-- updating
     fill(0xFF);
@@ -306,7 +427,7 @@ public class CaseSimplePID extends PApplet{
     pushStyle();
     {
       fill(0x55555555);
-      stroke(0xFFEEEEEE);
+      stroke(0xFFAAAAAA);
       rect(pxX, pxY, lpWidth, lpHeight);
       fill(0xFFEEEEEE);
       text(pxText, pxX+lpMargin, pxY+lpMargin);
@@ -414,15 +535,15 @@ public class CaseSimplePID extends PApplet{
     final int lpBarrelEastX = pxTarget.cmRegion.width*(lpDevide-2) / lpDevide;
     final int lpBarrelW     = pxTarget.cmRegion.width*(lpDevide-1) / lpDevide;
     final int lpSupporterW  = pxTarget.cmRegion.height /16;
-    final int lpBerrelH     = pxTarget.cmRegion.height *2 /3;
-    final int lpSupporterG  = (pxTarget.cmRegion.height - lpBerrelH)/2;
+    final int lpBarrelH     = pxTarget.cmRegion.height *2 /3;
+    final int lpSupporterG  = (pxTarget.cmRegion.height - lpBarrelH)/2;
     final int lpDuctG       = lpSupporterW;
     fill(pxTarget.cmBackColor);
     rect(//..barrel
       pxTarget.cmRegion.x,
       pxTarget.cmRegion.y + lpSupporterG,
       lpBarrelW,
-      lpBerrelH
+      lpBarrelH
     );
     rect(//..supporter west
       pxTarget.cmRegion.x + lpBarrelWestX,
@@ -444,7 +565,7 @@ public class CaseSimplePID extends PApplet{
       pxTarget.cmRegion.y,
       //--
       pxTarget.cmRegion.x +lpBarrelW +lpDuctG,
-      ccGetCenterY(pxTarget.cmRegion)
+      ccGetCenterY(pxTarget.cmRegion) -lpDuctG
     );
   }//+++
   
@@ -454,14 +575,26 @@ public class CaseSimplePID extends PApplet{
   
   class ZcReal{
     float cmVal;
-    boolean cmIsStatic;
+    boolean cmStatical;
+    public ZcReal(){
+      cmVal=0.0f;
+      cmStatical=false;  
+    }//++!
+    public ZcReal(float pxInitValue){
+      cmVal=pxInitValue;
+      cmStatical=false;  
+    }//++!
+    public ZcReal(float pxInitValue, boolean pxStatical){
+      cmVal=pxInitValue;
+      cmStatical=pxStatical;  
+    }//++!
   }//***
   
   void ccTransfer(ZcReal pxPotentialP, ZcReal pxPotentialN, int pxRatio){
     float lpDiff=pxPotentialP.cmVal-pxPotentialN.cmVal;
     lpDiff/=pxRatio;
-    if(!pxPotentialP.cmIsStatic){pxPotentialP.cmVal+=lpDiff;}
-    if(!pxPotentialN.cmIsStatic){pxPotentialN.cmVal-=lpDiff;}
+    if(!pxPotentialP.cmStatical){pxPotentialP.cmVal+=lpDiff;}
+    if(!pxPotentialN.cmStatical){pxPotentialN.cmVal-=lpDiff;}
   }//+++
   
   //=== controller
